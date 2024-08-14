c35c125cedef0d6e9111e5a4af15a7f436ded86a
Andres Zuleta <afleon386@misena.edu.co>
2024-08-14 15:30:44 -0500

    Se realizo la logica del login y ese enlace con el controlador de crearIngreso, por medio de las funcionalidad de jquery

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
    
-    $(document).on('click', '#btnIngresar', function(){
-       
-        $.ajax({
-            url: '../../controlador/crearIngresoControlador.php',
-            method: 'POST',
-            dataType: 'JSON',
-            data: $()
-        })
+$(document).on('click', '#btnIngresar', function(){
+    $.ajax({
+        url: 'controlador/crearIngresoControlador.php',
+        method: 'POST',
+        datatype: 'JSON',
+        data: $('#formLogin').serialize(),
+    }).done(function(json){
+      if(json != 0){
+        alert('Bienvenido')
+        window.location.href = 'vista/vistaEmpleado.php';
+      }
+      else{
+        alert('Usuario o contraseña incorrectos');
+        return false;
+      }
+
+    }).fail(function(error){
+       console.error('Error:', error);
     })
+})
 })
