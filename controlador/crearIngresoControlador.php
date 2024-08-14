3e1f339c31affe56f1f92cb462dc4dd519ab17f6
Andres Zuleta <afleon386@misena.edu.co>
2024-08-14 15:27:40 -0500

    Se modifico las rutas que contemplan los includes tanto de la entidad como modelo. Además, los new objetos de Empleado a empleadoEntidad y empleadoModelo

--- controlador/crearIngresoControlador.php
+++ controlador/crearIngresoControlador.php
@@ -1,15 +1,15 @@
 <?php
-include_once './entidad/empleadoEntidad.php';
-include_once './modelo/empleadoModelo.php';
+include_once '../entidad/empleadoEntidad.php';
+include_once '../modelo/empleadoModelo.php';
 
 $numero_documento = $_POST['numero_documento'];
 $contrasena = $_POST['contrasena'];
 
-$empleadoE = new entidad\Empleado();
+$empleadoE = new \entidad\empleadoEntidad();
 $empleadoE->setNumero_documento($numero_documento);
 $empleadoE->setContrasena($contrasena);
 
-$empleadoM = new modelo\Empleado($empleadoE);
+$empleadoM = new \modelo\empleadoModelo($empleadoE);
 $respuesta = $empleadoM->crearIngreso();
 echo json_encode($respuesta);
 
