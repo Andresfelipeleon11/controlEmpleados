bc25b52a361d2b5df8588a283c0082b59dc72aaf
Andres Zuleta <afleon386@misena.edu.co>
2024-08-14 15:25:55 -0500

    Se modifico la clase del modelo de Empleado a empleadoModelo, para evitar problemas.

--- modelo/empleadoModelo.php
+++ modelo/empleadoModelo.php
@@ -1,37 +1,49 @@
 <?php
 namespace modelo;
-include_once "./entorno/conexion.php";
 USE PDO;
-class Empleado {
+USE PDOException;
+include_once "../entorno/conexion.php";
+
+class empleadoModelo {
+    private $id_empleado;
     private $numero_documento;
     private $contrasena;
     public $con;
 
-    public function __construct(\entidad\Empleado $empleadoE)
+    public function __construct(\entidad\empleadoEntidad $empleadoE)
     {
+        $this->id_empleado = $empleadoE->getId_empleado();
         $this->numero_documento = $empleadoE->getNumero_documento();
         $this->contrasena = $empleadoE->getContrasena();
         $this->con = new \Conexion();
+        $this->con->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function crearIngreso(){
+       try{
         $sql = "SELECT * FROM tbl_empleado te WHERE numero_documento=:numero_documento AND contrasena=:contrasena";
-        $resultado = $this->con->conexion->prepare($sql);
-        $resultado->bindParam('numero_documento', $this->numero_documento);
-        $resultado->bindParam('contrasena', $this->contrasena);
-        $resultado->execute();
+        $result = $this->con->conexion->prepare($sql);
+        $result->bindParam('numero_documento', $this->numero_documento);
+        $result->bindParam('contrasena', $this->contrasena);
+        $result->execute();
         
-        $resultExit = $resultado->fetchAll(PDO::FETCH_ASSOC);
-        if(isset($resultExit) && !empty ($resultExit)){
+        $resultado = $result->fetchAll(PDO::FETCH_ASSOC);
+        if(isset($resultado) && !empty ($resultado)){
             session_start();
-            $_SESSION['numero_documento'] = $resultExit[0] ['numero_documento'];
-            $_SESSION['id'] = $resultExit[0] ['id'];
+            $_SESSION['numero_documento'] = $resultado[0] ['numero_documento'];
+            $_SESSION['id_empleado'] = $resultado[0] ['id_empleado'];
         }
         else{
-            return $resultExit;
+           $resultado = 0;  // si el resultado es 0, significa que el usuario no existe.
         }
-
+        return $resultado;
+       }
+       catch(PDOException $e){
+           return 'Error: ' . $e;
+       }
+       
     }
+    
 }
 
 
