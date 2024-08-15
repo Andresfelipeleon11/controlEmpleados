3bb3ef64bec527067eedb8b84e3d59a7d47b7f47
Andres Zuleta <afleon386@misena.edu.co>
2024-08-15 16:48:35 -0500

    Se realizan unos breves cambios en la logico dejamos aun lado la funcionalidad de jquery para capturar el evento del boton, en estos casos utilizamos addEventListener para escuchar el evento con su respectivo id de btnIngreso. Además, se formateo el codigo y validaciones con respecto al formulario ára evitar campos en blanco entre otras con forme al tiempo se irán añadiendo.

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,49 @@
-$(document).ready(function(){
-   
-$(document).on('click', '#btnIngresar', function(){
-    $.ajax({
-        url: 'controlador/crearIngresoControlador.php',
-        method: 'POST',
-        datatype: 'JSON',
-        data: $('#formLogin').serialize(),
-    }).done(function(json){
-      if(json != 0){
-        alert('Bienvenido')
-        window.location.href = 'vista/vistaEmpleado.php';
-      }
-      else{
-        alert('Usuario o contraseña incorrectos');
-        return false;
-      }
+$(document).ready(function () {
 
-    }).fail(function(error){
-       console.error('Error:', error);
-    })
-})
-})
+  //Boton Ingresar
+  let btnIngresar = document.querySelector("#btnIngresar");
+
+  //#Evento
+  btnIngresar.addEventListener("click", function () {
+    //variables del formulario
+    let numero_documento = document.querySelector("#numero_documento").value;
+    let contrasena = document.querySelector("#contrasena").value;
+
+    //#Validaciones
+    if (numero_documento == "") {
+      alert("Por favor, ingrese su número de identificación");
+      setInterval(window.location.reload(), 2000);
+      return false;
+    }
+    if (numero_documento.length > 10 || numero_documento.length < 10) {
+      alert("Por favor, ingrese un número de identificación válido");
+      setInterval(window.location.reload(), 2000);
+      return false;
+    }
+    if (contrasena == "") {
+      alert("Por favor, ingrese su contraseña");
+      setInterval(window.location.reload(), 2000);
+      return false;
+      
+    } else {
+      $.ajax({
+        url: "controlador/crearIngresoControlador.php",
+        method: "POST",
+        datatype: "JSON",
+        data: $("#formLogin").serialize(),
+      })
+        .done(function (json) {
+          if (json != 0) {
+            alert("Bienvenido");
+            window.location.href = "vista/vistaEmpleado.php";
+          } else {
+            alert("Usuario o contraseña incorrectos");
+            return false;
+          }
+        })
+        .fail(function (error) {
+          console.error("Error:", error);
+        });
+    }
+  });
+});
